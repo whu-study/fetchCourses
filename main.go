e4daf7745791333079b2b991e5d70a26c74c0f1c
forDecember <1921567337@qq.com>
2025-03-19 21:16:45 +0800

    update: jsons

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
-var cookie = "_dx_uzZo5y=1641785697592RSOSLdVXre7YXItZA6NtZp84qUZqdcgX; _dx_app_111=; _dx_captcha_vid=sl6m8oebm5tdepow; iPlanetDirectoryPro=xHUJ00bxlz6XwnPnW9jSau; SF_cookie_1=16391220; JSESSIONID=08316BABA0E11577DF4590BF9D59A7BC"
+var cookie = "_dx_uzZo5y=1641785697592RSOSLdVXre7YXItZA6NtZp84qUZqdcgX; _dx_app_111=; _dx_captcha_vid=sl6m8oebm5tdepow; SF_cookie_1=16391220; JSESSIONID=CE7F3A7D70E3AEC52FB3D29D6A93780A; iPlanetDirectoryPro=q0sa7YGfbRqmt7sFfEvWpu"
 
 func main() {
-	UpdateDB()
+	//UpdateDB()
 	//getMajor()
 	//getAllMajorDetails()
 
